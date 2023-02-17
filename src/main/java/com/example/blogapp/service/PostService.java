c1be4ba9cc6bde77316f2d680cf8f8b9f46ff211
Jeff <11721615+jeffellin@users.noreply.github.com>
2023-02-17 09:23:15 -0500

    fix reactive issue

--- src/main/java/com/example/blogapp/service/PostService.java
+++ src/main/java/com/example/blogapp/service/PostService.java
@@ -3,14 +3,8 @@ package com.example.blogapp.service;
 import com.example.blogapp.model.Post;
 import io.micrometer.observation.Observation;
 import io.micrometer.observation.ObservationRegistry;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.support.WebClientAdapter;
-import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.util.List;
 
@@ -20,16 +14,17 @@ public class PostService {
     private final RestTemplate restTemplate;
     private final ObservationRegistry registry;
 
-    public PostService(RestTemplate restTemplate, ObservationRegistry registry) {
+    private final JsonPostsService jps;
+
+    public PostService(RestTemplate restTemplate, ObservationRegistry registry, JsonPostsService jps) {
         this.restTemplate = restTemplate;
         this.registry = registry;
+        this.jps = jps;
     }
 
     public List<Post> loadPosts() {
 
-        WebClient client = WebClient.builder().baseUrl("https://jsonplaceholder.typicode.com").build();
-        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(client)).build();
-        JsonPostsService jps = factory.createClient(JsonPostsService.class);
+
 
         List<Post> posts = Observation
                 .createNotStarted("json-place-holder.load-posts", registry)
